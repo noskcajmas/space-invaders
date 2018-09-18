353dc14bd51c6ac0b6b61b0da46f92618e30bd42
noskcajmas <sjackson864@outlook.com>
2018-09-18 23:30:47 +0100

    Added boundaries

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode((1024, 768))
 img = pygame.image.load("Sprites/invader-1.png")
 x = 496
 y = 672
-speed = 10
+speed = 8
 
 running = True
 while running:
@@ -17,9 +17,9 @@ while running:
             running = False
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > speed:
         x -= speed
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 1024 - 72:
         x += speed
 
     screen.fill((0,0,0))
